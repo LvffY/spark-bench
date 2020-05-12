e84da5d6b17ca35bacb758658ca9a3505af89e4b
aberges <armand.berges@soprasteria.com>
2020-05-12 09:31:03 +0200

    ✨ Add tee cmd for output stdout in file

--- bin/sncf-bench.sh
+++ bin/sncf-bench.sh
@@ -7,7 +7,7 @@ for file in $(find ./examples/SNCF -type f -name *.conf | sort); do
   exec=$(find . -name spark-bench.sh)
   chmod +x $exec
   echo "$exec $file"
-  $exec $file 2>"spark-bench-$(basename $file .conf)-$execdate-spark.log"
+  $exec $file 2>"spark-bench-$(basename $file .conf)-$execdate-spark.log" | tee "spark-bench-$(basename $file .conf)-$execdate.log"
 done
 
 hdfs dfs -mkdir -p /tmp/spark-bench/logs/$execdate
